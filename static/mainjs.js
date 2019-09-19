0740fc1cade02aec448c220b23bf4cfa8c2e1495
Tomasz Soróbka <tomek.sorobka@gmail.com>
2019-09-19 20:39:58 +0200

    delete function

--- static/mainjs.js
+++ static/mainjs.js
@@ -1,13 +1,68 @@
-const trash = document.getElementsByClassName('fa-trash-alt')[0];
+// import { DH_NOT_SUITABLE_GENERATOR } from "constants";
 
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
+
+const trash = document.querySelectorAll('.fa-trash-alt');
+const edit = document.querySelectorAll('.fa-edit')
+console.log(trash);
 const addition = document.getElementById('additionButton');
 
 addition.addEventListener('click', addElement);
 
-// trash.addEventListener('click', removeElement);
+trash.forEach(function(elem) {
+    elem.addEventListener("click", removeElement)
+});
+
+edit.forEach(function(elem) {
+    elem.addEventListener("click", editElement)
+});
+
+
+
+let taskContainer = document.getElementById('taskContainer');
+let taskUl = document.getElementById('taskUl');
+
+function editElement (e) {
+    console.log(e)
+   let parent =  e.target.parentElement.parentElement;
+   console.log(parent)
+
+   parent.innerHTML = "";
+   parent.className = "list-group-item edit-item";
+//    input.contentEditable = 'true';
+   
+    let div = document.createElement('div');
+    let label = document.createElement('label');
+    let input = document.createElement('input');
+    let button = document.createElement('button');
+
+    div.className = "container";
+    label.className = "edit";
+    label.innerText = "Edit:";
+    input.type = "text";
+    input.className = "form-control edit";
+    button.className = "btn btn-outline-info btn-save";
+    button.type = "save";
+    button.innerText = "Save";
+
+
+    div.appendChild(label);
+    div.appendChild(input);
+    div.appendChild(button);
+    parent.appendChild(div);
+    
+    let buttonSave = parent.div.button;
+    
+}
 
 function removeElement(e) {
-    console.log(e);
+   let parent = e.target.parentElement.parentElement;
+   console.log(parent)
+   taskUl.removeChild(parent);
+   
+//    taskContainer.removeChild(parent);
 };
 
 function addElement (e) {
@@ -29,7 +84,6 @@ input.type = "checkbox";
 input.value = "";
 div.className = "fonts d-inline";
 
-
 div.appendChild(editIcon);
 div.appendChild(trashIcon);
 
@@ -40,7 +94,7 @@ span.innerHTML = addInput;
 label.appendChild(span);
 
 li.appendChild(div);
-
-let taskContainer = document.getElementById('taskContainer');
 taskContainer.appendChild(li);
-;}
+trashIcon.addEventListener('click', removeElement);
+};
+
