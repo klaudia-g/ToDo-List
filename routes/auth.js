91da4c4f917493b3e6c09141f7c38e22ebd3bab8
Magdalena Grad <mgdln.grad@gmail.com>
2019-09-21 12:01:48 +0200

    add auth middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,3 @@
-const config = require('config');
-const jwt = require('jsonwebtoken');
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
@@ -12,14 +10,13 @@ router.post('/', async(req, res)=>{
   const { error } = validate(req.body);
   if(error) return res.status(400).send(error.details[0].message);
 
-  let user = await User.findOne({ userName: req.body.userName })
+  let user = await User.findOne({ userName: req.body.userName });
   if (!user) return res.status(400).send('Invalid user name.');
 
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if(!validPassword) return res.status(400).send('Invalid password');
     
-    const token = jwt.sign({ _id: user._id}, config.get('jwtPrivateKey'));
-
+    const token = user.generateAuthToken();
     res.send(token);
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
+const auth = require('../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const {User, validate} = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
+
+router.get('/me', auth, async(req, res)=>{
+  const user = await User.findById(req.user._id).select('-password');
+  res.send(user);
+});
 
 router.post('/', async(req, res)=>{
   const { error } = validate(req.body);
@@ -17,7 +25,8 @@ router.post('/', async(req, res)=>{
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
-  res.send(_.pick(user, ['_id', 'userName', 'name']));
+  const token = user.generateAuthToken();
+  res.header('x-auth-token', token).send(_.pick(user, ['_id', 'userName', 'name']));
 });
 
 module.exports = router;
