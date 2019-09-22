146a09f45b0309cdb71753064babb04042402b6b
Zaneta <zaneta.ignatz@o2.pl>
2019-09-22 04:02:51 +0200

    tried to do requests but didn't work out well

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,16 @@
 import "../css/style.css";
 import { validateLogin, validateSignUp } from "./validation";
+import { createUser, logUser, logOut } from "./log";
+import { addPost, removePost, postTask, deleteTask } from "./taskfunctions";
+
 
 validateLogin();
 validateSignUp();
+createUser();
+logUser();
+logOut();
+addPost();
+removePost();
+postTask();
+deleteTask();
+
